79ee393532a8ff239995ced9d0af206771dcc7bd
neethuganesh25 <neethuganesh2001@gmail.com>
2024-07-30 23:50:30 +0530

    updated

---
+++ logic.js
@@ -0,0 +1,18 @@
+if(item.weather[0].main=='Clear'){
+    img.src = "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/cloud-64.png";
+ }
+else if(item.weather[0].main=='Rain'){
+    img.src = "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/cloud-64.png";
+}
+else if(item.weather[0].main=='Snow'){
+    img.src = "./Images/Snow.svg";
+}
+else if(item.weather[0].main=='Clouds'){
+    img.src = "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/cloud-64.png";
+}
+else if(item.weather[0].main=='Mist'){
+    img.src = "./Images/Mist.svg";
+}
+else if(item.weather[0].main=='Haze'){
+    img.src = "./Images/Haze.svg";
+}
